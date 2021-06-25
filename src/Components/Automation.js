d3e25a06f68c0d11a17720439cdfe5f2ff215002
Javay Porter <jporter@mhelpdesk.com>
2021-06-25 10:09:28 -0400

    update automation step

--- src/Components/Automation.js
+++ src/Components/Automation.js
@@ -1,72 +1,75 @@
-import React, { Component } from 'react';
-import {Grid, Cell, Tab, Tabs} from 'react-mdl';
-import './Automation.css';
+import React, { Component } from "react";
+import { Grid, Cell, Tab, Tabs } from "react-mdl";
+import "./Automation.css";
 
 const createGithubSteps = [
-    'Click the following link: https://github.com/',
-    'Enter a unique Username (i.e javayporterAL)',
-    'Enter AL email in Email field',
-    'Enter Password ',
-    'Click Sign up for GitHub'
-
-]
-const listCreateGitHubSteps = createGithubSteps.map((createGithubSteps) =>
+  "Click the following link: https://github.com/",
+  "Enter a unique Username (i.e javayporterAL)",
+  "Enter AL email in Email field",
+  "Enter Password ",
+  "Click Sign up for GitHub",
+];
+const listCreateGitHubSteps = createGithubSteps.map((createGithubSteps) => (
   <li>{createGithubSteps}</li>
-)
-;
-
+));
 class Automation extends Component {
-    render() {
-        return(
+  render() {
+    return (
+      <div style={{ width: "100%", margin: "auto" }}>
+        <div className="demo-tabs">
+          <Tabs>
+            <Tab>Front End</Tab>
+            <Tab>Backend</Tab>
+            <Tab>Mobile</Tab>
+          </Tabs>
+        </div>
 
-            
-<div style ={{width: '100%', margin: 'auto'}}>
+        <Grid className="demo-grid-1">
+          <Cell className="rounded-border" col={6}>
+            <h5>Creating an AL GitHub account</h5>
+            <h6>
+              <ul>{listCreateGitHubSteps}</ul>
+            </h6>
+          </Cell>
 
-<div className="demo-tabs">
-    <Tabs>
-        <Tab>Front End</Tab>
-        <Tab>Backend</Tab>
-        <Tab>Mobile</Tab>
-    </Tabs>
-</div>      
+          <Cell className="rounded-border" col={6}>
+            <h5>Locating AL_PY_AUTOMATION Repo</h5>
+            <h6>
+              <ul>{listCreateGitHubSteps}</ul>
+            </h6>
+          </Cell>
 
+          <Cell className="rounded-border" col={6}>
+            <h5>Cloning the AL_PY_AUTOMATION Repo</h5>
+            <h6>
+              <ul>{listCreateGitHubSteps}</ul>
+            </h6>
+          </Cell>
 
-<Grid className="demo-grid-1">
-    <Cell className="rounded-border" col={6}><h5>Creating an AL GitHub account</h5>
-        <h6><ul>{listCreateGitHubSteps}</ul></h6>
-    </Cell>
+          <Cell className="rounded-border" col={6}>
+            <h5>Download an IDLE</h5>
+            <h6>
+              <ul>{listCreateGitHubSteps}</ul>
+            </h6>
+          </Cell>
 
-    <Cell className="rounded-border" col={6}><h5>Locating AL_PY_AUTOMATION Repo</h5>
-        <h6><ul>{listCreateGitHubSteps}</ul></h6>
-    </Cell>
+          <Cell className="rounded-border" col={6}>
+            <h5>Switching Branches</h5>
+            <h6>
+              <ul>{listCreateGitHubSteps}</ul>
+            </h6>
+          </Cell>
 
-    <Cell className="rounded-border" col={6}><h5>Cloning the AL_PY_AUTOMATION Repo</h5>
-        <h6><ul>{listCreateGitHubSteps}</ul></h6>
-    </Cell>
-
-    <Cell className="rounded-border" col={6}><h5>Download VS Studio Code</h5>
-        <h6><ul>{listCreateGitHubSteps}</ul></h6>
-    </Cell>
-
-    <Cell className="rounded-border" col={6}><h5>Switching Branches</h5>
-        <h6><ul>{listCreateGitHubSteps}</ul></h6>
-    </Cell>
-
-    <Cell className="rounded-border"  col={6}><h5>Installing NPM and Homebrew</h5>
-        <h6><ul>{listCreateGitHubSteps}</ul></h6>
-    </Cell>
-
-</Grid>
-    
-  
-</div>
-
-                
-            
-        )
-    }
+          <Cell className="rounded-border" col={6}>
+            <h5>Installing NPM and Homebrew</h5>
+            <h6>
+              <ul>{listCreateGitHubSteps}</ul>
+            </h6>
+          </Cell>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default Automation;
-
-
